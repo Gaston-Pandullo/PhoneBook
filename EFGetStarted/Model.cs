bf55a467134f63705d2eb7030d9aa4d8d25d645d
Gaston Pandullo <gpandullo3@gmail.com>
2025-05-03 16:35:53 -0300

    Agregar archivos de proyecto.

---
+++ EFGetStarted/Model.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata;
+
+public class BloggingContext : DbContext
+{
+    DbSet<Blog> Blog { get; set; }
+    DbSet<Post> Post { get; set; }
+    public string DbPath { get; }
+    public BloggingContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = System.IO.Path.Join(path, "blogging.db");
+    }
+}
+
+public class Blog
+{
+    public int BlogId { get; set; }
+    public string BlogUrl { get; set; }
+    public List<Post> ListPosts { get; } = new();
+}
+public class Post
+{
+    public int PostId { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+
+    public int BlogId { get; set; }
+    public Blog Blog { get; set; }
+}
